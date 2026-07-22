7b62dfea36add29eb563ce3f7f97d587476d0b7a
Birmap2314 <kolesnikov27092012@gmail.com>
2026-07-22 13:29:41 +0300

    Update ASd112e3.lua

--- ASd112e3.lua
+++ ASd112e3.lua
@@ -1,2 +1,1 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Birmap2314/HUJFIUHSUIFDH/refs/heads/main/hkver"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Birmap2314/HUJFIUHSUIFDH/refs/heads/main/prota.lua"))()
